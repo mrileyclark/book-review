e65660d5cae5ce46042c2146a623c17c76d336b6
mclark <maryariley4578@yahoo.com>
2024-03-24 17:16:54 -0400

    adding added changes to js files

--- app.js
+++ app.js
@@ -45,3 +45,93 @@ secEl.innerHTML = `
       </div>
 `;
 main2.appendChild(secEl);
+
+//**********************
+
+// gets first 30 results of popular movies
+
+//search by popularity with my own api key
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=63aaf07e1c839389dad481b7331b0094&page=1";
+//image of movie
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+//allows to search
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=63aaf07e1c839389dad481b7331b0094&page=1&query="';
+
+//references these elements
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+//get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  // gets data
+  const data = await res.json();
+  console.log(data);
+  showMovies(data.results);
+}
+
+//shows movies on main page
+function showMovies(movies) {
+  //clears main
+  main.innerHTML = "";
+
+  //gives access to variiable from movie that comes in
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    //create movie div with data
+    const movieEl = document.createElement("div");
+    //gives initial div
+    movieEl.classList.add("movie");
+
+    //creates movie UI
+    movieEl.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" 
+      alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+    `;
+    //puts it into main so display
+    main.appendChild(movieEl);
+  });
+}
+
+//get rating of movies
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+//listens for submit in search bar
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  //checks serachTerm exists and not empty
+  if (searchTerm && searchTerm !== "") {
+    //search movies by the searchTerm
+    getMovies(SEARCH_API + searchTerm);
+    //clears search
+    search.value = "";
+  } else {
+    //reloads page
+    window.location.reload();
+  }
+});
